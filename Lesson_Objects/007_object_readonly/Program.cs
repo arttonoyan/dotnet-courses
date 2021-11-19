32a3ae7202330620f9ada17e541f8e59dcaa342d
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:38:47 +0400

    Add lesson 7

---
+++ Lesson_Objects/007_object_readonly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _007_object_readonly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson_Objects/007_object_readonly/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace _007_object_readonly
+{
+    class Student
+    {
+        public Student(string email)
+        {
+            this.email = email;
+        }
+
+        public readonly string email;
+
+        public string name;
+        public string surname;
+        public int age;
+    }
+
+}
