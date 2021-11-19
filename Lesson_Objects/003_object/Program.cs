c2dfcc062c62f340aa63174985367653774c506f
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:28:26 +0400

    Add lesson 3

---
+++ Lesson_Objects/003_object/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _003_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = null;
+            Student st2 = null;
+
+            if (object.ReferenceEquals(st1, st2))
+            {
+
+            }
+
+            //int age1 = st1.age;
+            if (st1 != null)
+            {
+                int age = st1.age;
+            }
+
+            //int age = st1?.age;
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+    }
+}
