0fe30856e9b5b0f166ce9ed132a4b44b64e39ed0
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 19:38:25 +0400

    Add Lessons 13 and 14

---
+++ Lesson_Objects/013_object_HashCode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace _013_object_HashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st1 = new Student { name = "A1", email = "a1@gmail.com" };
+            var st2 = new Student { name = "A1", email = "a1@gmail.com" };
+
+            if(st1.Equals(st2))
+            {
+
+            }
+
+            var set = new HashSet<Student>();
+            set.Add(st1);
+            set.Add(st2);
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public string email;
+
+        public override int GetHashCode()
+        {
+            return email.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var st = obj as Student;
+            if (st == null)
+                return false;
+
+            return email == st.email;
+        }
+
+        public override string ToString()
+        {
+            return email;
+        }
+    }
+}
---
+++ Lesson_Objects/014_object_HashCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _014_object_HashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st1 = new Student { name = "A1", email = "a1@gmail.com" };
+            var st2 = new Student { name = "A1", email = "a1@gmail.com" };
+
+            if (st1.Equals(st2))
+            {
+
+            }
+
+            var set = new HashSet<Student>(new StudentEqualityComparer());
+            set.Add(st1);
+            set.Add(st2);
+        }
+    }
+
+    class StudentEqualityComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x == null && y == null)
+                return false;
+
+            if (x == null || y == null)
+                return false;
+
+            return x.email == y.email;
+        }
+
+        public int GetHashCode([DisallowNull] Student obj)
+        {
+            return obj.email.GetHashCode();
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public string email;
+    }
+}
