7557107f59d3a77d7bbd34e1a2d3b011558fa945
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:26:56 +0400

    Ad lesson 2

---
+++ Lesson_Objects/002_object/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _002_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new();
+            st1.name = "A1";
+
+            Type type = st1.GetType();
+            int code = st1.GetHashCode();
+
+            Student st3 = st1;
+
+            Student st2 = new();
+            st2.name = "A1";
+
+            if (st1.Equals(st2))
+            {
+
+            }
+
+            if (st3.Equals(st1))
+            {
+
+            }
+        }
+    }
+
+    class Student
+    {
+        public string name;
+    }
+}
