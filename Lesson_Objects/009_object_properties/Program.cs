0857e0ef44dac71b0ab4aa451dce56a7378fbf68
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 15:13:27 +0400

    Add lesson 9

--- Lesson_Objects/009_object_properties/Program.cs
+++ Lesson_Objects/009_object_properties/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace _008_object_properties
+﻿namespace _009_object_properties
 {
     class Program
     {
@@ -8,11 +8,13 @@
             {
                 name = "A1",
                 surname = "A1yan",
-                //age = -25896
+                Age = -25896
             };
 
-            st.SetAge(-25896);
-            int age = st.GetAge();
+            st.Age = -25896;
+            int age = st.Age;
+            //st.SetAge(-25896);
+            //int age = st.GetAge();
 
         }
     }
---
+++ Lesson_Objects/009_object_properties/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace _009_object_properties
+{
+    class Student
+    {
+        public Student(string email)
+        {
+            this.email = email;
+        }
+
+        public string name;
+        public string surname;
+        public readonly string email;
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (14 >= value || value > 50)
+                    age = 0;
+                else
+                    age = value;
+            }
+        }
+
+        //public int get_Age()
+        //{
+        //    return age;
+        //}
+
+        //public void set_Age(int value)
+        //{
+        //    if (14 >= value || value > 50)
+        //        age = 0;
+        //    else
+        //        age = value;
+        //}
+    }
+}
