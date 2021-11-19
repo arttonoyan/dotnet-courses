7c51b99b451b12027e3894afab5dc6baaccc129b
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:31:07 +0400

    Add lesson 4

---
+++ Lesson_Objects/004_object/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _004_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new();
+            st1.name = "A1";
+
+            Student st2 = new();
+            st2.name = "A2";
+
+            st1.Test();
+            st2.Test();
+
+            Student st3 = null;
+            st3.Test();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+
+        public void Test()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
