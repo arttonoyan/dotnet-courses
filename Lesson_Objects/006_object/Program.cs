6d73ebc5151733e0c31bbc21a3bfbc49aa0072b0
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:35:54 +0400

    Add Lesson 6

---
+++ Lesson_Objects/006_object/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _006_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student st = new Student(
+            //    name: "A1", 
+            //    surname: "A1yan", 
+            //    age: 18);
+
+            Student st = new()
+            {
+                name = "A1",
+                surname = "A1yan",
+                email = "a1@gmail.com",
+                age = 18
+            };
+
+        }
+    }
+}
---
+++ Lesson_Objects/006_object/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace _006_object
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public string email;
+        public int age;
+    }
+
+}
