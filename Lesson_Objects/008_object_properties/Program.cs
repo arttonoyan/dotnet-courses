ad524b6288ae47db9e343f49ba3686ac64e16e09
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 15:11:39 +0400

    Add lesson 8

---
+++ Lesson_Objects/008_object_properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _008_object_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new("a1@gmail.com")
+            {
+                name = "A1",
+                surname = "A1yan",
+                //age = -25896
+            };
+
+            st.SetAge(-25896);
+            int age = st.GetAge();
+
+        }
+    }
+}
---
+++ Lesson_Objects/008_object_properties/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace _008_object_properties
+{
+    class Student
+    {
+        public Student(string email)
+        {
+            this.email = email;
+        }
+
+        public string name;
+        public string surname;
+        public readonly string email;
+        private int age;
+
+        public void SetAge(int value)
+        {
+            if (14 >= value || value > 50)
+                age = 0;
+            else
+                age = value;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+
+}
