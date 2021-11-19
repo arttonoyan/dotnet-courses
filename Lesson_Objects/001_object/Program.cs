466a1e2be4b3b4e93f1c2212dbec6bc37d66cb48
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-19 13:22:19 +0400

    Create solutuin and add firt lesson.

---
+++ Lesson_Objects/001_object/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _001_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+
+            string text = st1.ToString();
+            int[] arr = new int[10];
+
+            Console.WriteLine(arr);
+            Console.WriteLine(st1);
+            Console.ReadLine();
+        }
+    }
+
+    class Student { }
+}
