6063d3658776db328f82bf4c55b2c2a7112835fd
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-22 13:08:27 +0400

    [MemberwiseClone] Add sample 16

---
+++ Lesson_Objects/016_object_MemberwiseClone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _016_object_MemberwiseClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st1 = new Student { Email = "a1@gmail.com", Name = "A1", Surname = "B1" };
+            var st2 = st1.Clone();
+
+            if (st1.Equals(st2))
+            {
+
+            }
+
+            Console.WriteLine($"Name:     {st2.Name}");
+            Console.WriteLine($"Surname:  {st2.Surname}");
+            Console.WriteLine($"Email:    {st2.Email}");
+        }
+    }
+}
---
+++ Lesson_Objects/016_object_MemberwiseClone/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _016_object_MemberwiseClone
+{
+    class Student
+    {
+        public string Email { get; init; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Fullname => $"{Surname} {Name}";
+
+        public Student Clone() =>
+            MemberwiseClone() as Student;
+    }
+}
