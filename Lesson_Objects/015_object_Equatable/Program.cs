4e2427b711b5c46aaae0d7d34283a6f3efae0e35
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-22 13:02:56 +0400

    Add sample 15

---
+++ Lesson_Objects/015_object_Equatable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _015_object_Equatable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st1 = new Student { Email = "a1@gmail.com", Name = "A1", Surname = "B1" };
+            var st2 = new Student { Email = "a1@gmail.com", Name = "A1", Surname = "B1" };
+
+            if (st1.Equals(st2))
+            {
+
+            }
+        }
+    }
+}
---
+++ Lesson_Objects/015_object_Equatable/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _015_object_Equatable
+{
+    class Student : IEquatable<Student>
+    {
+        public string Email { get; init; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Fullname => $"{Surname} {Name}";
+
+        public bool Equals(Student other)
+        {
+            if (other == null)
+                return false;
+
+            return Email == other.Email;
+        }
+    }
+}
