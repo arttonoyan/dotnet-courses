194e17153c28348763bfdd407aa8130772daa0aa
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-29 17:23:49 +0400

    Add project 5

---
+++ Lesson_Nested/005_Nested/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _005_Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Nested.StaticMethodFromNested();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyClass
+    {
+        public static class Nested
+        {
+            public static void StaticMethodFromNested()
+            {
+                Console.WriteLine("Static method from Nested class.");
+            }
+        }
+    }
+}
