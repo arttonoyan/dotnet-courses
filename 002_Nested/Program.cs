fe54598ad02083b955a00ce760a6647bad363790
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-29 17:18:03 +0400

    Add project 2

---
+++ 002_Nested/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _002_Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new MyClass.Nested();
+            instance.ChangeField(10);
+
+            MyClass.PrintField();
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        private static int field = 0;
+
+        public static void PrintField()
+        {
+            Console.WriteLine("field: " + field);
+        }
+
+        public class Nested
+        {
+            public void ChangeField(int a)
+            {
+                field = a;
+
+                Console.WriteLine(field);
+            }
+        }
+    }
+}
