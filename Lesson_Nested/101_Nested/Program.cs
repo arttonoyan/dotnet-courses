393d0169945cb0ec249539e03879d468685c7dbc
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-29 17:14:23 +0400

    Add project Nested

---
+++ Lesson_Nested/101_Nested/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _101_Nested
+{
+    class Program
+    {
+        static void Main()
+        {
+            var instance = new MyClass.Nested();
+
+            instance.Method();
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        public class Nested
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method from Nested class.");
+            }
+        }
+    }
+}
