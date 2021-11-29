0d2dc0afcf79046f67ad3271feb7df575e04a6de
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-29 17:22:53 +0400

    Add project 4

---
+++ Lesson_Nested/004_Nested/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _004_Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new MyClass.Nested();
+            instance.MethodFromNested();
+
+            MyClass.StaticMethod();
+            MyClass.StaticMethod();
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class MyClass
+    {
+        static MyClass()
+        {
+            Console.WriteLine("MyClass Static Constructor");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static method from MyClass");
+        }
+
+        public class Nested
+        {
+            public void MethodFromNested()
+            {
+                Console.WriteLine("Method from Nested class.");
+            }
+        }
+    }
+}
