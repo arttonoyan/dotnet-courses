d01eb8b9ea62f8f6c93e63acc639173487ac7786
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 15:04:19 +0400

    [Struct] Add lesson 8, boxing

---
+++ Lesson_Structure/008_struct_boxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _008_struct_boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            // Boxing
+            object o = a;
+
+            // Unboxing
+            int a1 = (int)o;
+
+            long l = a;
+            long l1 = (long)(int)o;
+            //long l2 = (int)o;
+
+            Console.ReadLine();
+        }
+    }
+}
