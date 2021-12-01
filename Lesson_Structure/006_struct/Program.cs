ab0a8a17878f92d57057b6e4abb174001b0b1248
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 14:57:56 +0400

    [Struct] Add lesson 6

---
+++ Lesson_Structure/006_struct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _006_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ms = new MyStruct { field = 20 }; //new MyStruct(20);
+            Console.WriteLine(ms.field);
+            //MyStruct.a = 10;
+            Console.ReadLine();
+        }
+    }
+
+    struct MyStruct
+    {
+        static MyStruct()
+        {
+            Console.WriteLine("Static Constructor.");
+        }
+
+        public MyStruct(int value)
+        {
+            Console.WriteLine("Constructor");
+            this.field = value;
+        }
+
+        public int field;
+        public static int a;
+    }
+
+}
