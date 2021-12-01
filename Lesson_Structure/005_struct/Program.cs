8afcf5635b8a21c85429720efabd62d5f2d7a4f8
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 14:54:46 +0400

    [Struct] Add lesson 5

---
+++ Lesson_Structure/005_struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _005_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyStruct.field = 10;
+            MyStruct.Show();
+
+            Console.ReadLine();
+        }
+    }
+
+    /*static*/ struct MyStruct
+    {
+        public static int field { get; set; }
+
+        public static void Show()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+}
