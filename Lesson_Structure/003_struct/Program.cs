09f7dfb632abe17945fe19d0a215ca759b8b32a2
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 14:44:16 +0400

    [Struct] Add lesson 3

---
+++ Lesson_Structure/003_struct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _003_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyStruct ms = new MyStruct();
+            var ms = new MyStruct(10);
+            //ms.field = 10;
+            Console.WriteLine(ms.field); // Can Use.
+
+            Console.ReadLine();
+        }
+    }
+
+    struct MyStruct
+    {
+        // Can't use default constructor.
+        //public MyStruct()
+        //{ }
+
+        public MyStruct(int value)
+        {
+            this.field = value; // Can't add comment
+            field1 = 0;
+        }
+
+        public int field;
+        public int field1;
+    }
+}
