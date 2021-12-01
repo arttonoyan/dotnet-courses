3d4cdc681ec7ee54d896208a9e46cbfa43a14c78
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 14:40:02 +0400

    Add first project of struct

---
+++ Lesson_Structure/001_struct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _001_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //ValueType vt = a;
+            MyStruct ms;
+
+            ms.field = 10;
+
+            Console.WriteLine(ms.field);
+
+            Console.ReadLine();
+
+        }
+    }
+
+    struct MyStruct
+    {
+        public int field;
+    }
+
+}
