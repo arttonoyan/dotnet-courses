e7703960ea220812306f45726d1c0207fc5bfc83
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-01 14:41:31 +0400

    [Struct] Add project 2

---
+++ Lesson_Structure/002_struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _002_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ms = new MyStruct();
+            ms.Field = 10;
+            Console.WriteLine(ms.Field);
+
+            Console.ReadLine();
+        }
+    }
+
+    struct MyStruct
+    {
+        private int field;
+        public int Field
+        {
+            get { return field; }
+            set { /*field = value;*/ }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(field);
+        }
+    }
+}
