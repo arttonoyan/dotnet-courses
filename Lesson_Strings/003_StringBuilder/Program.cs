15967c229bf119e3f5f032c7af197a9463e61332
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-10 15:14:09 +0400

    Add lesson StringBuilder

---
+++ Lesson_Strings/003_StringBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _003_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("StringBuilder ").Append("is ").Append("very ").Append("fast ... ");
+
+            string build1 = builder.ToString();
+
+            builder.Append("faster");
+
+            string build2 = builder.ToString();
+
+            Console.WriteLine(build1);
+            Console.WriteLine(build2);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_Strings/004_StringBuilder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _004_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int count = 100000;
+            string simpleString = "";
+
+            DateTime time = DateTime.Now;
+
+            for (int i = 0; i < count; i++)
+            {
+                simpleString += "a";
+            }
+
+            TimeSpan timeSS = DateTime.Now - time;
+
+            Console.WriteLine("Common string build time {0}.", timeSS);
+
+            var builder = new StringBuilder();
+
+            time = DateTime.Now;
+
+            for (int i = 0; i < count; i++)
+            {
+                builder.Append("a");
+            }
+
+            TimeSpan timeSB = DateTime.Now - time;
+
+            Console.WriteLine("StringBuilder build time {0}.", timeSB);
+
+            Console.WriteLine("Common string length: {0}", simpleString.Length);
+
+            simpleString = builder.ToString();
+
+            Console.WriteLine("StringBuilder length: {0}", simpleString.Length);
+
+            //Delay.
+            Console.ReadKey();
+        }
+    }
+}
