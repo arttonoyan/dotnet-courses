f9ddface8bc0df0d39e6c3ce2181b1ec5e915b8f
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-13 13:03:56 +0400

    [Strings] Add lesson 5

---
+++ Lesson_Strings/005_Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _005_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "SomeText";
+            string s2 = "SomeText";
+            string s3 = s2;
+            s2 = "NewText";
+
+            if (s1.Equals(s2))
+            {
+
+            }
+
+            if (ReferenceEquals(s1, s3))
+            {
+
+            }
+
+            var someText = Console.ReadLine();
+            if (s1.Equals(someText))
+            {
+
+            }
+
+            someText = string.Intern(someText);
+
+            if (ReferenceEquals(s1, someText))
+            {
+
+            }
+
+            string res = "";
+            res += "a1";
+            res += "a2";
+            res += "a3";
+            res += "a4";
+
+            //Intern Table
+            //a1
+            //a2
+            //a1a2
+            //a3
+            //a1a2a3
+            //a4
+            //a1a2a3a4
+        }
+    }
+}
