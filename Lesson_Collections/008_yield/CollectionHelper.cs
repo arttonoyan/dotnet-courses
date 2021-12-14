e1ef934f057c83e9aa952379fc4d92e8ffbfe914
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:54:12 +0400

    [Enumerable] Add Lesson8

---
+++ Lesson_Collections/008_yield/CollectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _008_yield
+{
+    static class CollectionHelper
+    {
+        public static IEnumerable<string> CreateEnumerable()
+        {
+            yield return "Hello";
+            yield return "Hello";
+            yield return "Hello";
+            yield return "Hello";
+            yield return "Hello";
+        }
+    }
+}
---
+++ Lesson_Collections/008_yield/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _008_yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string item in CollectionHelper.CreateEnumerable())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
