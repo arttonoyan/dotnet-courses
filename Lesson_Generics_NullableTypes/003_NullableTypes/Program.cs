41d8e0d1057d34066b99526f5a58b37e5abf4096
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:57:45 +0400

    [NullableTypes] Add Lesson3

---
+++ Lesson_Generics_NullableTypes/003_NullableTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _003_NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int? b;
+
+            b = a ?? 10; // b = 10
+
+            // b = (a != null) ? a : 10;
+
+            //if (a == null)
+            //    b = 10;
+            //else
+            //    b = a;
+
+            Console.WriteLine(b);
+
+            a = 5;
+            b = a ?? 10; // b = 5
+
+            Console.ReadLine();
+        }
+    }
+}
