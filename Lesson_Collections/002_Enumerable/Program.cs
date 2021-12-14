2bbcf5ec476a2a17dace10d061817225112513ee
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:52:48 +0400

    [Enumerable] Add Lesson2

---
+++ Lesson_Collections/002_Enumerable/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _002_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode rootNode = new ListNode { value = 10 };
+            rootNode
+                .Add(20)
+                .Add(30)
+                .Add(40);
+        }
+    }
+
+    class ListNode
+    {
+        public int value;
+        public ListNode next;
+
+        public ListNode Add(int item)
+        {
+            next = new ListNode { value = item };
+            return next;
+        }
+
+        public override string ToString() => $"{value}, next: {next?.value}";
+    }
+}
