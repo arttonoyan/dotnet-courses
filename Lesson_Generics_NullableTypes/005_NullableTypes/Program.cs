29a6c979cbb13145d22ec285ceb660669e356192
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:58:01 +0400

    [NullableTypes] Add Lesson5

---
+++ Lesson_Generics_NullableTypes/005_NullableTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _005_NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> a = 10; // struct
+
+            if (a.HasValue)
+            {
+                Console.WriteLine("a is {0}.", a.Value);
+            }
+
+            int? b = 10;
+            if (b.HasValue)
+            {
+                Console.WriteLine("a is {0}.", a.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
