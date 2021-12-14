c3444cb7f270bb45710624c222f2c7c6bdfe9dd2
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:57:36 +0400

    [NullableTypes] Add Lesson2

---
+++ Lesson_Generics_NullableTypes/002_NullableTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _002_NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int? b = 10;
+
+            // Expression is always null
+            if (a > b)
+            {
+                Console.WriteLine("a > b");
+            }
+            else
+            {
+                Console.WriteLine("a < b");
+            }
+
+            // Expression is always null
+            if (a == null)
+            {
+                Console.WriteLine("a == b");
+            }
+            else
+            {
+                Console.WriteLine("a != b");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
