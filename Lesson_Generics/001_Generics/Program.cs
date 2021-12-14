b4dc0ae69a1b2cde658ab93a5380493fc5fa877c
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:55:39 +0400

    [Generics] Add Lesson1

---
+++ Lesson_Generics/001_Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _001_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> instance1 = new MyClass<int>();
+            instance1.Method();
+
+            MyClass<long> instance2 = new MyClass<long>();
+            instance2.Method();
+
+            MyClass<string> instance3 = new MyClass<string>();
+            instance3.field = "ABC";
+            instance3.Method();
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass<T>
+    {
+        public T field;
+
+        public void Method()
+        {
+            Console.WriteLine(field.GetType());
+        }
+    }
+}
