0e3769f2b9c27784ea534df7b2593bddfaec96a0
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:57:50 +0400

    [NullableTypes] Add Lesson4

---
+++ Lesson_Generics_NullableTypes/004_NullableTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _004_NullableTypes
+{
+    class Program
+    {
+        private static int? index;
+
+        static void Main(string[] args)
+        {
+            if (index == null)
+            {
+                Console.WriteLine("null");
+            }
+            else if(index == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine("other...");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
