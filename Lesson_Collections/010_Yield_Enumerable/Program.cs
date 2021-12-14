3abde683652cf874953cdced32471fd6984039e7
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:54:38 +0400

    [Enumerable] Add Lesson10

---
+++ Lesson_Collections/010_Yield_Enumerable/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _010_Yield_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listNode = new ListNode<int> { 10, 20, 1000, 40 };
+            foreach (int item in listNode)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class ListNode<T> : IEnumerable<T>
+    {
+        public T value;
+        public ListNode<T> next;
+        private ListNode<T> _last;
+
+        public ListNode<T> Add(T item)
+        {
+            if (_last == null)
+            {
+                next = new ListNode<T> { value = item };
+                _last = next;
+            }
+            else
+            {
+                _last.next = new ListNode<T> { value = item };
+                _last = _last.next;
+            }
+
+            return _last;
+        }
+
+        public override string ToString() => $"{value}, next: {next.value}";
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this;
+            while(node != null)
+            {
+                yield return node.value;
+                node = node.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
