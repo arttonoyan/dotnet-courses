1a79e0a40afb41dc57db9b025bb061fc6e18a1a4
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:56:27 +0400

    [Generics] Add Lesson3

---
+++ Lesson_Generics/003_Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _003_Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+
+            instance.Method<string>("Hello world!");
+
+            instance.Method("Hello world!");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        public void Method<T>(T argument)
+        {
+            T variable = argument;
+
+            Console.WriteLine(variable);
+        }
+    }
+}
