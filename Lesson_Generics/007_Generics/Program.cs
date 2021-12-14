742c07a02c960068f4c683324b20923553e87d62
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 10:57:00 +0400

    [Generics] Add Lesson7

---
+++ Lesson_Generics/007_Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+//covariant, contravariant, invariant
+namespace _007_Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            Shape shape = new Rectangle();
+
+            IContainer<Rectangle> container = new Container<Shape>(shape);
+
+            Console.WriteLine(container.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Shape { }
+    public class Rectangle : Shape { }
+
+    public interface IContainer<in T>
+    {
+        T Figure { set; }
+    }
+
+    public class Container<T> : IContainer<T>
+    {
+        private T figure;
+
+        public Container(T figure)
+        {
+            this.figure = figure;
+        }
+
+        public T Figure
+        {
+            set { figure = value; }
+        }
+
+        public override string ToString()
+        {
+            return figure.GetType().ToString();
+        }
+    }
+}
