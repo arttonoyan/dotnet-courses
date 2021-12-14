53ed0ce62e4bb3d378070d03aa332f2483abbc62
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:55:31 +0400

    [Enumerable] Add Lesson12

---
+++ Lesson_Collections/012_Yield_Stream/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _012_Yield_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
+
+            var allBytes1 = stream.AsEnumerable().ToArray();
+            var allBytes2 = File.ReadAllBytes("test.txt");
+
+            bool isTrue = Equals(allBytes1, allBytes2);
+
+            stream.Dispose();
+
+            Console.ReadLine();
+        }
+
+        private static bool Equals(byte[] bytes1, byte[] bytes2)
+        {
+            if (bytes1.Length != bytes2.Length)
+                return false;
+
+            for (int i = 0; i < bytes1.Length; i++)
+            {
+                if (bytes1[i] != bytes2[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Lesson_Collections/012_Yield_Stream/StreamExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _012_Yield_Stream
+{
+    static class StreamExtensions
+    {
+        public static IEnumerable<byte> AsEnumerable(this Stream source)
+        {
+            return Repeat(source.ReadByte).TakeUntil(-1).Select(p => (byte)p);
+        }
+
+        public static IEnumerable<byte> FirstLine(this Stream source)
+        {
+            return Repeat(source.ReadByte).TakeUntil('\n').Or(-1).Select(p => (byte)p);
+        }
+
+        public static IEnumerable<List<byte>> Lines(this Stream source)
+        {
+            while (true)
+            {
+                var en = FirstLine(source).ToList();
+                if (en.Count == 0)
+                    yield break;
+                else
+                {
+                    yield return en;
+                    source.Position++;
+                }
+            }
+        }
+
+        public static IEnumerable<int> Or(this IEnumerable<int> source, int value)
+        {
+            return TakeUntil(source, value);
+        }
+
+        public static IEnumerable<int> TakeUntil(this IEnumerable<int> source, int value)
+        {
+            foreach (var item in source)
+            {
+                if (item == value)
+                    yield break;
+                else
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<T> Repeat<T>(this Func<T> func)
+        {
+            while (true)
+                yield return func.Invoke();
+        }
+    }
+
+}
