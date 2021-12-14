cd0becf3b1641837a002a65050fd3351191d21be
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:55:14 +0400

    [Enumerable] Add Lesson11

---
+++ Lesson_Collections/011_Yield_DataRepository/IDataRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _011_Yield_DataRepository
+{
+    public interface IDataRepository
+    {
+        IEnumerable<TModel> ExecuteAsEnumerable<TModel>(string query, 
+            Func<IDataRecord, TModel> convertFunc) 
+            where TModel : class, new();
+    }
+
+    class DataRepository : IDataRepository
+    {
+        public DataRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private readonly string _connectionString;
+
+        public IEnumerable<TModel> ExecuteAsEnumerable<TModel>(string query, Func<IDataRecord, TModel> mapper)
+            where TModel : class, new()
+        {
+            using var conn = new SqlConnection(_connectionString);
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+
+            using var comand = conn.CreateCommand();
+            comand.CommandType = CommandType.Text;
+            comand.CommandText = query;
+            var reader = comand.ExecuteReader();
+
+            while (reader.Read())
+                yield return mapper.Invoke(reader);
+
+            //TModel model;
+            //while (reader.Read())
+            //{
+            //    try
+            //    {
+            //        model = mapper.Invoke(reader);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        //log
+            //        continue;
+            //    }
+
+            //    yield return model;
+            //}
+        }
+    }
+}
---
+++ Lesson_Collections/011_Yield_DataRepository/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace _011_Yield_DataRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "...";
+            IDataRepository repo = new DataRepository("connString");
+
+            Func<IDataRecord, SomeClass> mapper = GetSomeClassMapper();
+            List<SomeClass> res = repo
+                .ExecuteAsEnumerable("select * from [Table] where .....", mapper)
+                .ToList();
+        }
+
+        static Func<IDataRecord, SomeClass> GetSomeClassMapper()
+        {
+            return reader => new SomeClass();
+        }
+    }
+
+    class SomeClass { }
+}
