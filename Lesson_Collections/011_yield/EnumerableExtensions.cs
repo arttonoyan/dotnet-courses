a73931d29423815391efe9a7152b8219f3a97339
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-14 20:54:58 +0400

    [Enumerable] Add Lesson11

---
+++ Lesson_Collections/011_yield/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace _011_yield
+{
+    static class EnumerableExtensions
+    {
+        public static IEnumerable<int> EvenNumbers(this IEnumerable<int> source)
+        {
+            foreach (int item in source)
+                if (item % 2 == 0)
+                    yield return item;
+        }
+
+        public static IEnumerable<int> GreaterThen(this IEnumerable<int> source, int number)
+        {
+            foreach (int item in source)
+                if (item > number)
+                    yield return item;
+        }
+
+        //public static List<int> EvenNumbers(this List<int> source)
+        //{
+        //    List<int> items = new List<int>();
+        //    foreach (int item in source)
+        //    {
+        //        if (item % 2 == 0)
+        //            items.Add(item);
+        //    }
+        //    return items;
+        //}
+
+        //public static List<int> GreaterThen(this List<int> source, int number)
+        //{
+        //    List<int> items = new List<int>();
+        //    foreach (int item in source)
+        //    {
+        //        if (item > number)
+        //            items.Add(item);
+        //    }
+        //    return items;
+        //}
+    }
+}
---
+++ Lesson_Collections/011_yield/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _011_yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> en1 = CreateRandomNumbers(100);
+
+            IEnumerable<int> en2 = en1
+                .EvenNumbers()
+                .GreaterThen(100);
+
+            List<int> list = en2.ToList();
+        }
+
+        static IEnumerable<int> CreateRandomNumbers(int count)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+                yield return rnd.Next(0, 150);
+        }
+    }
+}
