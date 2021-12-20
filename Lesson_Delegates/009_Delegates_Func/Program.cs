fef04450da34ad2a80341c1ee59ecb202a0d3dad
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:42:55 +0400

    [Delegates] Add Lesson9, Func

---
+++ Lesson_Delegates/009_Delegates_Func/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _009_Delegates_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> GetMax = arr =>
+            {
+                int max = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                        max = arr[i];
+                }
+
+                return max;
+            };
+
+            Func<int, int[]> GenerateArr = n =>
+            {
+                int[] arr = new int[n];
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                    arr[i] = rnd.Next(10, 100);
+
+                return arr;
+            };
+
+            int[] arr1 = GenerateArr(10);
+            int max1 = GetMax(arr1);
+
+            Console.ReadLine();
+        }
+    }
+}
