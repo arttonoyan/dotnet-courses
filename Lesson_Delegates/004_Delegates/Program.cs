e64fe3e39d8377351e0bbd153a4d8d4c6c60c29b
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:42:16 +0400

    [Delegates] Add Lesson4

---
+++ Lesson_Delegates/004_Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _004_Delegates
+{
+    internal class Program
+    {
+        //Anonymous Methods
+        static void Main(string[] args)
+        {
+            //MyAction myAction = new MyAction(delegate 
+            //{
+            //    Console.WriteLine("Hello Delegate!!!");
+            //});
+
+            myAction = delegate { Console.WriteLine("Hello Delegate!!!"); };
+            CallDel();
+            //myAction.Invoke();
+            //myAction.Invoke();
+
+            Console.ReadLine();
+        }
+
+        public static MyAction myAction;
+
+        public static void CallDel()
+        {
+            myAction.Invoke();
+        }
+
+        public delegate void MyAction();
+    }
+}
