bf754ba444ab6dc6d2bd5e169986b0f40ef589ab
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:33:20 +0400

    [Delegates] Add Lesson1

---
+++ Lesson_Delegates/001_Delegates/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _001_Delegates
+{
+    public class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Call Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Call Method2");
+        }
+    }
+}
---
+++ Lesson_Delegates/001_Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _001_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            var myAction = new MyAction(new MyClass().Method1);
+            var myAction1 = new MyAction(myClass.Method2);
+
+            myAction1.Invoke();
+            //myAction.Invoke();
+
+            //myAction();
+
+            Console.ReadLine();
+        }
+
+        private delegate void MyAction();
+    }
+}
