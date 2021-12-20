3755f3fde7e4f17cfae0f5d5692f92a849b4bf4a
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:42:44 +0400

    [Delegates] Add Lesson8

---
+++ Lesson_Delegates/008_Delegates_Func/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _008_Delegates_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> plus = (a1, a2) => a1 + a2;
+
+            Console.WriteLine("10 + 5 = " + plus(10, 5));
+            Console.ReadLine();
+        }
+    }
+}
