d64c53f2d4c18b54cc6036f84a3f4f0a109b651c
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:42:28 +0400

    [Delegates] Add Lesson6

---
+++ Lesson_Delegates/006_Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _006_Delegates
+{
+    // Lambda Expression
+    internal class Program
+    {
+        public delegate int MyAction(int a1, int b1);
+
+        static void Main(string[] args)
+        {
+            MyAction plus = (a, b) => a + b;
+            MyAction minus = (a, b) => a - b;
+
+            int res1 = plus(10, 5) + plus(10, 5);
+            int res2 = minus(10, 5);
+
+            #region
+
+            MyAction action;
+
+            // Lambda Method
+            action = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            Console.WriteLine("Lambda Method: 10 + 5 = " + action(10, 5));
+
+            // Lambda Operator
+            action = (a, b) =>
+            {
+                return a + b;
+            };
+            Console.WriteLine("Lambda Operator: 10 + 5 = " + action(10, 5));
+
+            // Lambda Expression
+            action = (a, b) => a + b;
+            Console.WriteLine("Lambda Expression: 10 + 5 = " + action(10, 5));
+
+            // Difference betweens "Lambda Operator" and "Lambda Expression"
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
