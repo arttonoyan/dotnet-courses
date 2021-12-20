f8b08a038ed4842029d4ec25e01107f741c597b4
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-20 08:42:11 +0400

    [Delegates] Add Lesson3

---
+++ Lesson_Delegates/003_Delegates/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _003_Delegates
+{
+    public class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Call Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Call Method2");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Call Method3");
+        }
+
+        public string Method4<T>(T a)
+        {
+            return a.ToString();
+        }
+    }
+}
---
+++ Lesson_Delegates/003_Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _003_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new MyClass();
+
+            var myAction1 = new MyAction(instance.Method1);
+            var myAction2 = new MyAction(instance.Method2);
+            var myAction3 = new MyAction(instance.Method3);
+
+            //myAction1();
+
+            //MyAction myAction = myAction2 + myAction1 + myAction3;
+            //MyAction myAction = myAction1 + myAction2 + myAction3;
+            MyAction myAction = myAction2 + myAction2 + myAction3;
+            myAction.Invoke();
+            myAction -= myAction3;
+
+            myAction.Invoke();
+
+            //myAction();
+
+            Console.ReadLine();
+        }
+
+        public delegate void MyAction();
+    }
+}
