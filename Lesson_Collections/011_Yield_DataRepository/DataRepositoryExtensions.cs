4c56753cb608edf4a85de9d2e019b429d4e990ed
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-21 08:39:50 +0400

    Implement AsyncExecuter

--- Lesson_Collections/011_Yield_DataRepository/DataRepositoryExtensions.cs
+++ Lesson_Collections/011_Yield_DataRepository/DataRepositoryExtensions.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace _011_Yield_DataRepository
 {
@@ -10,5 +13,17 @@ namespace _011_Yield_DataRepository
         public static IEnumerable<TModel> GetExecuter<TModel>(this IDataRepository repository, string query, Func<IDataRecord, TModel> mapper)
             where TModel : class, new()
             => repository.GetExecuter(query).Select(mapper);
+
+        public static IAsyncEnumerable<TModel> GetAsyncExecuter<TModel>(this IDataRepository repository, string query, Func<IDataRecord, TModel> mapper, CancellationToken cancellationToken = default)
+            where TModel : class, new()
+            => repository.GetAsyncExecuter(query).SelectAsync(mapper, cancellationToken);
+
+        public static async IAsyncEnumerable<TResult> SelectAsync<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TResult> selector, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            await foreach (var item in source.WithCancellation(cancellationToken))
+            {
+                yield return selector(item);
+            }
+        }
     }
 }
--- Lesson_Collections/011_Yield_DataRepository/IDataRepository.cs
+++ Lesson_Collections/011_Yield_DataRepository/IDataRepository.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace _011_Yield_DataRepository
 {
     public interface IDataRepository
     {
         IEnumerable<IDataRecord> GetExecuter(string query);
+        IAsyncEnumerable<IDataRecord> GetAsyncExecuter(string query, CancellationToken cancellationToken = default);
     }
 
     class DataRepository : IDataRepository
@@ -32,6 +35,21 @@ namespace _011_Yield_DataRepository
             var reader = comand.ExecuteReader();
 
             while (reader.Read())
+                yield return reader;
+        }
+
+        public async IAsyncEnumerable<IDataRecord> GetAsyncExecuter(string query, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            if (conn.State != ConnectionState.Open)
+                await conn.OpenAsync(cancellationToken);
+
+            using var comand = conn.CreateCommand();
+            comand.CommandType = CommandType.Text;
+            comand.CommandText = query;
+            var reader = await comand.ExecuteReaderAsync(cancellationToken);
+
+            while (await reader.ReadAsync(cancellationToken))
                 yield return reader;
         }
     }
