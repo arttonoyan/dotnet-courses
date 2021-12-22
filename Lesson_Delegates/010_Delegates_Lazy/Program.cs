8554e9f84952c57048574618678b637ac056b14d
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-22 08:45:06 +0400

    Add Lesson Lazy (Empty project)

---
+++ Lesson_Delegates/010_Delegates_Lazy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _010_Delegates_Lazy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson_Delegates/011_Delegates_Lazy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _011_Delegates_Lazy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
