a34ac1d32b0f3aa138da82524b76d08e0ad84c22
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-22 08:46:53 +0400

    Add Lesson 2, AnonymousType

---
+++ Lesson_Linq/002_AnonymousType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _002_AnonymousType
+{
+    //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/anonymous-types
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new
+            {
+                Name = "A1",
+                Age = 36
+            };
+
+            //student.Name = "";
+
+            Type type = student.GetType();
+            Console.WriteLine(type);
+
+            Console.ReadLine();
+        }
+    }
+}
