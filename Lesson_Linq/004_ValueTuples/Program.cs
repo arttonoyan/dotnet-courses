4e008eb1b05144eacba63b71e79669de7156cd31
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-22 08:52:28 +0400

    Add Lesson 4, TupleTuples

---
+++ Lesson_Linq/004_ValueTuples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _004_ValueTuples
+{
+    //https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-tuples
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+
+            (string Name, int Year) o1 = ("A1", dt.Year);
+            (string Name, int Year) o2 = ("A1", dt.Year);
+
+            if (o1.Equals(o2))
+            {
+
+            }
+
+            var o3 = o1;
+            if (ReferenceEquals(o1, o3))
+            {
+
+            }
+
+            var o4 = ("A1", dt.Year);
+            var o5 = (Name: "A1", dt.Year);
+        }
+    }
+}
