d7293006e3cb869df7cda72ed38ae9f5893c4c89
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-24 08:24:22 +0400

    Add Lesson1, Reflection

---
+++ Lesson_Reflection/001_Type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _001_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var my = new MyClass();
+
+            Type t1 = my.GetType();
+            Type t2 = typeof(MyClass);
+            Type t3 = Type.GetType("_001_Type.MyClass");
+
+            bool isClass = t1.IsClass;
+            bool isValueType = t1.IsValueType;
+            bool isAbstract = t1.IsAbstract;
+            bool isArray = t1.IsArray;
+            bool isGenericType = t1.IsGenericType;
+            Type baseType = t1.BaseType;
+        }
+    }
+
+    public class MyClass
+    { }
+}
