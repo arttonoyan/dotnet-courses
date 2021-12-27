a321190db81ba44abdabede09c9396472d9a7680
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-27 11:12:00 +0400

    [Attributes] Add Lesson 2

---
+++ Lesson_Attributes/002_Attributes/MyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _002_Attributes
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    class MyAttribute : Attribute
+    {
+        public MyAttribute(string date)
+        {
+            this.date = date;
+        }
+
+        private readonly string date;
+        public string Date
+        {
+            get { return date; }
+        }
+
+        public int Number { get; set; }
+    }
+}
---
+++ Lesson_Attributes/002_Attributes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace _002_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyClass my = new MyClass();
+            //MyClass.Method();
+
+            Type type = typeof(MyClass);
+            object[] attributes = null;
+
+            MyAttribute attribute = null;
+
+            // Analysis of type attributes
+            // Get all attributes of a given Attribute type (false  - without checking base classes).
+            attributes = type.GetCustomAttributes(false);
+
+            foreach (object attributeType in attributes)
+            {
+                attribute = attributeType as MyAttribute;
+                Console.WriteLine("Type Analysis  : Number = {0}, Date = {1}", attribute.Number, attribute.Date);
+            }
+
+            // Analysis of method attributes.
+            // Get public static method - Method.
+            MethodInfo method = type.GetMethod("Method", BindingFlags.Public | BindingFlags.Static);
+
+            // Get all attributes of a given Attribute type (false - without checking base classes).
+            attributes = method.GetCustomAttributes(typeof(MyAttribute), false);
+
+            foreach (MyAttribute attrib in attributes)
+            {
+                Console.WriteLine("Анализ метода: Number = {0}, Date = {1}", attrib.Number, attrib.Date);
+            }
+
+            // Delay.
+            Console.ReadLine();
+        }
+    }
+
+    [My("5/28/2016", Number = 1)]
+    public class MyClass
+    {
+        [Description("adsjfhskj")]
+        [My("5/31/2016", Number = 2)]
+        public static void Method()
+        {
+            Console.WriteLine("MyClass.Method()\n");
+        }
+    }
+}
