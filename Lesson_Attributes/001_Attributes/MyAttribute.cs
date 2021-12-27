871b39606865d19d5cb67d18a5deb31f306c154f
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-27 11:11:55 +0400

    [Attributes] Add Lesson 1

---
+++ Lesson_Attributes/001_Attributes/MyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _001_Attributes
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    class MyAttribute : Attribute
+    {
+        public MyAttribute(string date)
+        {
+            this.date = date;
+        }
+
+        public MyAttribute(string date, int number)
+            : this(date)
+        {
+            Number = number;
+        }
+
+        private readonly string date;
+        public string Date
+        {
+            get { return date; }
+        }
+
+        public int Number { get; set; }
+    }
+}
---
+++ Lesson_Attributes/001_Attributes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _001_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var my = new MyClass();
+            MyClass.Method();
+
+            Console.WriteLine();
+        }
+    }
+
+    [My("5/29/2016", Number = 1)]
+    public class MyClass
+    {
+        [My("2/31/2007", 10)]
+        public static void Method()
+        {
+            Console.WriteLine("MyClass.Method()\n");
+        }
+    }
+}
