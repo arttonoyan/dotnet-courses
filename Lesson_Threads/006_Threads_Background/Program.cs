bf84388ecb5d36bfd6db9f4c1f558d11c221f884
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-03-25 16:27:13 +0400

    [Threads] Add Lesson6  Background

---
+++ Lesson_Threads/006_Threads_Background/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace _006_Threads_Background
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(Worker);
+
+            // Turning a thread into a background thread
+            t.IsBackground = false;
+            t.Start();
+
+            //Thread.Sleep(2000);
+            Console.WriteLine("Returning from Main");
+        }
+
+        private static void Worker()
+        {
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //10 Second
+            //Thread.Sleep(10000);
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(".");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Returning from Worker");
+        }
+    }
+}
