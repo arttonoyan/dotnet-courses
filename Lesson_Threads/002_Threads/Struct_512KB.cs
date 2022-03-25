9a4f924ba0f72200768f673b8ed0ed4613511d9f
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-03-25 15:56:09 +0400

    Fix Lesson_Threads folder name

---
+++ Lesson_Threads/002_Threads/Struct_512KB.cs
@@ -0,0 +1,10 @@
+﻿namespace _002_Threads
+{
+    struct Struct_512KB
+    {
+        public Struct_128KB a1;
+        public Struct_128KB a2;
+        public Struct_128KB a3;
+        public Struct_128KB a4;
+    }
+}
