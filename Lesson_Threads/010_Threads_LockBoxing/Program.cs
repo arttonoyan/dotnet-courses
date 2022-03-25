db21acdd67d1866c71c8d6bc886b42d1c23411a6
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-03-25 16:36:21 +0400

    Add Lesson 10 Threads_LockBoxing

---
+++ Lesson_Threads/010_Threads_LockBoxing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace _010_Threads_LockBoxing
+{
+    internal class Program
+    {
+        private static int counter = 0;
+
+        static void Main()
+        {
+            var thread1 = new Thread(Worker);
+            thread1.Start();
+
+            var thread2 = new Thread(Worker);
+            thread2.Start();
+
+            var thread3 = new Thread(Worker);
+            thread3.Start();
+
+            Console.ReadLine();
+        }
+
+        private static int block = 0;
+
+        static private void Worker()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                Monitor.Enter((object)block);
+                try
+                {
+                    Console.WriteLine(++counter);
+                }
+                finally
+                {
+                    Monitor.Exit((object)block);
+                }
+            }
+        }
+    }
+}
