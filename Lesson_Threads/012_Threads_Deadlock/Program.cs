9fb61e83c267c92bdbbdb0ded1301a6bfbef22af
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-03-25 16:39:41 +0400

    Add Lesson 12 Threads_Deadlock

---
+++ Lesson_Threads/012_Threads_Deadlock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _012_Threads_Deadlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
