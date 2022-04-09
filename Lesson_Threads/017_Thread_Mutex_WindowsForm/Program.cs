ee5e353254a45be8632d4ccaeadd0c9e673e227f
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-09 18:53:33 +0400

    Add lesson 17_Thread_Mutex_WindowsForm

---
+++ Lesson_Threads/017_Thread_Mutex_WindowsForm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _017_Thread_Mutex_WindowsForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            const string appName = "Lesson SingletonApp";
+            using (var mutex = new Mutex(false, appName, out bool isNotCreated))
+            {
+                if(isNotCreated)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+            }
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
