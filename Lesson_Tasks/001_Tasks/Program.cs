2a69d244786da8a7749e47d23f9240b5923c9279
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-18 08:39:31 +0400

    Add lesson 001_Tasks

---
+++ Lesson_Tasks/001_Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _001_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            // Create a task and supply a user delegate by using a lambda expression. 
+            var taskA = new Task(() => Console.WriteLine("Hello from taskA."));
+            // Start the task.
+            taskA.Start();
+
+            // Output a message from the calling thread.
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+
+            Console.ReadLine();
+
+        }
+    }
+}
