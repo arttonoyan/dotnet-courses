0a024e9a16b9b4d29fe6920d0551eea66336251d
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-20 01:05:23 +0400

    Add 006_Tasks_ContinueWith

---
+++ Lesson_Tasks/006_Tasks_ContinueWith/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _006_Tasks_ContinueWith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskA = new Task(() =>
+            {
+                Console.WriteLine("Hello from taskA.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                for (int count = 0; count < 50; count++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write("-");
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            });
+
+            var taskb = taskA.ContinueWith(t =>
+            {
+                Console.WriteLine("Hello from taskB.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int count = 0; count < 50; count++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write("^");
+                }
+
+                Console.ResetColor();
+                Console.WriteLine();
+            });
+
+            taskA.Start();
+            taskb.Wait();
+
+            Console.WriteLine("Main thread has been completed.");
+
+            Console.ReadLine();
+        }
+    }
+}
