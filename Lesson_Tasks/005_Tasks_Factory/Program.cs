51bd59e9220bccc2d21f76be7270c7f6109de315
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-20 00:58:05 +0400

    Add lesson 005_Tasks_Factory

---
+++ Lesson_Tasks/005_Tasks_Factory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _005_Tasks_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread start.");
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("MyTask() # " + Task.CurrentId + " Start.");
+
+                for (int count = 0; count < 10; count++)
+                {
+                    Thread.Sleep(500);
+
+                    Console.WriteLine("MyTask() #" + Task.CurrentId + " count: " + count);
+                }
+
+                Console.WriteLine("MyTask() # " + Task.CurrentId + " Complete.");
+            });
+
+            task.Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("Main thread has been completed. CurrentId = null? - {0}.", Task.CurrentId == null);
+
+            Console.ReadLine();
+
+        }
+    }
+}
