2cfc9f8a47ceac923ef9b104262c3e4a7722ec27
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-20 16:02:32 +0400

    Ad Lesson 011_Tasks_ConfigureAwait

---
+++ Lesson_Tasks/011_Tasks_ConfigureAwait/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _011_Tasks_ConfigureAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var syncContext = SynchronizationContext.Current;
+            //var syncContextType = syncContext?.ToString();
+
+            textBox1.Text = GetDataAsync().Result.ToString();
+        }
+
+        public static async Task<int> GetDataAsync()
+        {
+            using var client = new HttpClient();
+
+            var data = await client
+                .GetByteArrayAsync("https://github.com/");
+                //.ConfigureAwait(false);
+
+            return data.Length;
+        }
+    }
+}
