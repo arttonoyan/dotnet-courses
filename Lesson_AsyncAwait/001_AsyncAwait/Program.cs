b27cf2300c3ef6d9ce6c99b14e401bba7c5f8073
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-29 12:35:40 +0400

    Add lesson 001_AsyncAwait

---
+++ Lesson_AsyncAwait/001_AsyncAwait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _001_AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            var my = new MyClass();
+            my.OperationAsync();
+
+            Console.ReadLine();
+        }
+
+        public class MyClass
+        {
+            public void Operation()
+            {
+                Console.WriteLine($"Operation ThreadId {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine("Start");
+                Thread.Sleep(2000);
+                Console.WriteLine("End");
+            }
+
+            public async void OperationAsync()
+            {
+                var task = new Task(Operation);
+                task.Start();
+                await task;
+            }
+        }
+    }
+}
