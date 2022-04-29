dd6ae2152a1b0362f1dd831001afcb2e25e93882
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-29 13:19:26 +0400

    Add Lesson 005_AsyncAwait

---
+++ Lesson_AsyncAwait/005_AsyncAwait/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _005_AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            var my = new MyClass();
+            my.OperationAsync();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyClass
+    {
+        public int Operation()
+        {
+            Console.WriteLine($"Operation ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+            return 100;
+        }
+
+        public void OperationAsync()
+        {
+            Task<int> task = Task<int>.Factory.StartNew(Operation);
+            TaskAwaiter<int> awaiter = task.GetAwaiter();
+            Action continuation = () => Console.WriteLine($"Result : {awaiter.GetResult()}");
+            awaiter.OnCompleted(continuation);
+        }
+
+        public async void OperationAsync1()
+        {
+            Task<int> task = Task<int>.Factory.StartNew(Operation);
+            Console.WriteLine($"Result : {await task}");
+        }
+    }
+}
