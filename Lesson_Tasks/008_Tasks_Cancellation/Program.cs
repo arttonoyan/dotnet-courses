4caf0f496e2a45c3eec4ac274a912280846d08a7
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-29 12:32:31 +0400

    Change name

--- Lesson_Tasks/008_Tasks_Cancellation/Program.cs
+++ Lesson_Tasks/008_Tasks_Cancellation/Program.cs
@@ -7,41 +7,39 @@ namespace _008_Tasks_Cancellation
     {
         static void Main(string[] args)
         {
-            var cts = new CancellationTokenSource();
+            using var cts = new CancellationTokenSource();
             CancellationToken token = cts.Token;
 
             // User defined Class with its own method for cancellation
-            var obj1 = new CancelableObject("1");
-            var obj2 = new CancelableObject("2");
-            var obj3 = new CancelableObject("3");
+            var obj1 = new Worker("1");
+            var obj2 = new Worker("2");
+            var obj3 = new Worker("3");
 
             // Register the object's cancel method with the token's
             // cancellation request.
-            token.Register(() => obj1.Cancel());
-            token.Register(() => obj2.Cancel());
-            token.Register(() => obj3.Cancel());
+            token.Register(() => obj1.Dispose());
+            token.Register(() => obj2.Dispose());
+            token.Register(() => obj3.Dispose());
 
             Console.WriteLine("Cancellation set in token source...");
             //Request cancellation on the token.
             cts.Cancel();
-            // Call Dispose when we're done with the CancellationTokenSource.
-            cts.Dispose();
 
             Console.ReadLine();
 
         }
     }
 
-    class CancelableObject
+    class Worker
     {
         public string id;
 
-        public CancelableObject(string id)
+        public Worker(string id)
         {
             this.id = id;
         }
 
-        public void Cancel()
+        public void Dispose()
         {
             Console.WriteLine("Object {0} Cancel callback", id);
             // Perform object cancellation here.
