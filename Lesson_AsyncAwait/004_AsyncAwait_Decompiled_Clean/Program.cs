ee54e32affc73d7bb66893662ebc1374df1b78ca
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-04-29 13:18:06 +0400

    Add Lesson 004_AsyncAwait_Decompiled_Clean

---
+++ Lesson_AsyncAwait/004_AsyncAwait_Decompiled_Clean/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _004_AsyncAwait_Decompiled_Clean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            var my = new MyClass();
+            my.OperationAsync();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyClass
+    {
+        public void Operation()
+        {
+            Console.WriteLine($"Operation ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Begin");
+            Thread.Sleep(2000);
+            Console.WriteLine("End");
+        }
+
+        public void OperationAsync()
+        {
+            AsyncStateMachine stateMachine = new();
+            stateMachine.outer = this;
+            stateMachine.builder = AsyncVoidMethodBuilder.Create();
+            stateMachine.state = -1;
+            stateMachine.builder.Start(ref stateMachine);
+        }
+
+        [CompilerGenerated]
+        [StructLayout(LayoutKind.Auto)]
+        private class AsyncStateMachine : IAsyncStateMachine
+        {
+            public MyClass outer;
+            public AsyncVoidMethodBuilder builder;
+            public int state;
+
+            void IAsyncStateMachine.MoveNext()
+            {
+                if (state == -1)
+                {
+                    Console.WriteLine($"OperationAsync (Part 1) ThreadId {Thread.CurrentThread.ManagedThreadId}\n");
+
+                    var task = new Task(outer.Operation);
+                    task.Start();
+
+                    state = 0;
+                    TaskAwaiter awaiter = task.GetAwaiter();
+                    var stateMachine = this;
+                    builder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+                    return;
+                }
+
+                Console.WriteLine($"\nOperationAsync (Part 2) ThreadId {Thread.CurrentThread.ManagedThreadId}");
+            }
+
+            void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                this.builder.SetStateMachine(stateMachine);
+            }
+        }
+    }
+}
