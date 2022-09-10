2a9d9761242f57b5b0d97783d4eb706f500411b0
Artyom Tonoyan <artonoyan@servicetitan.com>
2022-09-10 12:00:42 +0400

    Update 009_Generics_Contravariance sample

--- Lesson_Generics/009_Generics_Contravariance/Program.cs
+++ Lesson_Generics/009_Generics_Contravariance/Program.cs
@@ -4,12 +4,63 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace _009_Generics_Contravariance
 {
+    public class MySet<T>
+    {
+        private readonly List<T> list = new List<T> ();
+        private readonly IMyEqualityComparer<T> comparer;
+
+        public MySet(IMyEqualityComparer<T> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public void Add(T value)
+        {
+            bool contains = false;
+            foreach (var item in list)
+            {
+                if(comparer.Equals(item, value))
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (!contains)
+                list.Add(value);
+        }
+    }
+
+    public interface IMyEqualityComparer<in T>
+    {
+        bool Equals(T? x, T? y);
+
+        int GetHashCode([DisallowNull] T obj);
+    }
+
+    public class MyPersonEqualityComparer : IMyEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y) =>
+            x.Email == y.Email;
+
+        public int GetHashCode([DisallowNull] Person obj) =>
+            obj.Email.GetHashCode();
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             var equalityComparer = new PersonEqualityComparer();
             IEnumerable<Person> people = new List<Employee>();
+
+            IMyEqualityComparer<Employee> employeeComparer = new MyPersonEqualityComparer();
+            var set = new MySet<Employee>(employeeComparer);
+            set.Add(new Employee { Email = "a1@gmail.com" });
+            set.Add(new Employee { Email = "a1@gmail.com" });
+            set.Add(new Employee { Email = "a1@gmail.com" });
+            set.Add(new Employee { Email = "a1@gmail.com" });
 
             var setEmployees = new HashSet<Employee>(equalityComparer)
             {
