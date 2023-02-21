c8784aa01c83f60647ac80357b03c8c647b5b089
Artyom Tonoyan <arttonoyan@gmail.com>
2023-02-21 16:49:27 +0400

    Add new lesson

---
+++ Lesson_Exceptions/016_Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Common;
+using System.IO;
+
+namespace _016_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class MyDataReader : TextReader
+    {
+        private readonly DbConnection _connection;
+
+        public MyDataReader(DbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (_connection != null)
+                _connection.Dispose();
+        }
+    }
+}
