a448539350ae405d84c0cd2fc3e90fc528876b8d
Artyom Tonoyan <arttonoyan@gmail.com>
2023-03-24 09:51:32 +0400

    Create sample for 005_Threads_Priorities

--- Lesson_Threads/005_Threads_Priorities/Program.cs
+++ Lesson_Threads/005_Threads_Priorities/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace _005_Threads_Priorities
 {
     internal class Program
     {
+        [ThreadStatic]
+        private static int count = 0;
+        private static volatile bool breack = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var threads = Enum.GetValues(typeof(ThreadPriority))
+                .Cast<ThreadPriority>()
+                .OrderByDescending(p => p)
+                .Select(priority => new Thread(Worker) { Priority = priority, Name = priority.ToString() })
+                .ToList();
+
+            threads.ForEach(t => t.Start());
+
+            Thread.Sleep(2000);
+            breack = true;
+        }
+
+        private static void Worker()
+        {
+            while (!breack)
+            {
+                count++;
+            }
+
+            decimal value = (decimal)count / 1000;            
+            Console.WriteLine($"{Thread.CurrentThread.Name} - {Math.Round(value, 0)}");
         }
     }
 }
