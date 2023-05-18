601fc9ecedec7d45af674755ab87047497397ff0
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-18 17:38:42 +0400

    Add 001_List

---
+++ Lesson_DataStructures/List/001_List/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _001_List;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<int>(528435456);
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            list.Add(i);
+        }
+
+        Console.ReadLine();
+    }
+}
