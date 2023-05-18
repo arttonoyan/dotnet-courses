69c03c7fe5466af8d627884b8a98f4f1c5db967d
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-18 17:43:15 +0400

    Add 004_List_Insert lesson

---
+++ Lesson_DataStructures/List/004_List_Insert/ArrayList.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+
+namespace _004_List_Insert;
+
+public class ArrayList<T> : IEnumerable
+{
+    public ArrayList() : this(0)
+    { }
+
+    public ArrayList(int capacity)
+    {
+        if (capacity < 0)
+        {
+            throw new ArgumentException("Error length");
+        }
+
+        _items = new T[capacity];
+    }
+
+    public ArrayList(ICollection<T> list)
+    {
+        _items = new T[list.Count];
+        Count = list.Count;
+        list.CopyTo(_items, 0);
+    }
+
+    private T[] _items;
+    private const int _defaultCapacity = 4;
+
+    public int Count { get; private set; }
+    public int Capacity
+    {
+        get { return _items.Length; }
+        set
+        {
+            if (value != _items.Length)
+            {
+                if (value > 0)
+                {
+                    T[] newItems = new T[value];
+                    Array.Copy(_items, 0, newItems, 0, _items.Length);
+                    _items = newItems;
+                }
+                else
+                {
+                    _items = new T[0];
+                }
+            }
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (_items.Length == Count)
+        {
+            EnsureCapacity();
+        }
+
+        _items[Count++] = item;
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index > Count)
+            throw new IndexOutOfRangeException();
+
+        if (_items.Length == Count)
+            EnsureCapacity();
+
+        Array.Copy(_items, index, _items, index + 1, Count - index);
+
+        _items[index] = item;
+        Count++;
+    }
+
+    private void EnsureCapacity()
+    {
+        int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+        Capacity = newCapacity;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return _items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+}
---
+++ Lesson_DataStructures/List/004_List_Insert/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _004_List_Insert;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list1 = new ArrayList<int> { 10, 20, 30 };
+        list1.Add(40);
+        list1.Insert(0, 50);
+        list1.Add(60);
+
+        var list2 = new List<int>();
+        list2.Capacity = 10;
+
+        if (list2.Contains(10))
+        {
+
+        }
+    }
+}
