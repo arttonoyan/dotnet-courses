16ae8bbd3af38457ed4a7898697269f521c573d7
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-23 14:43:25 +0400

    Add 001_Stack

---
+++ Lesson_DataStructures/Stack/001_Stack/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _001_Stack;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //268435456 -> 1GB
+        var stack = new Stack<int>();
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            stack.Push(i);
+        }
+    }
+}
