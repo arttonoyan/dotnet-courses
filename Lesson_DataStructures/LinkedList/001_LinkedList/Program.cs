f38152a0b2541e29508ce2ccaf263510ef306abc
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-23 14:04:00 +0400

    Add 001_LinkedList

---
+++ Lesson_DataStructures/LinkedList/001_LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _001_LinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //152867621
+        var list = new LinkedList<int>();
+        LinkedListNode<int> node = list.AddLast(1);
+
+        LinkedListNode<int> node2 = new LinkedListNode<int>(2);
+        list.AddLast(node2);
+
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            //list.AddLast(i);
+            list.AddFirst(i);
+        }
+    }
+}
