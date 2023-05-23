4cfa62fde59a51a6d8ae49ddb6338df5367d5ebc
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-23 14:49:04 +0400

    Add 004_Stack_TraverseTree

---
+++ Lesson_DataStructures/Stack/004_Stack_TraverseTree/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _004_Stack_TraverseTree;
+
+//https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-iterate-through-a-directory-tree
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TraverseTree(@"C:\Users\user\source\repos");
+        TraverseTree(@"C:\Users\artonoyan\source\repos\dotnet-courses\Lesson_DataStructures\Stack\004_Stack_TraverseTree");
+
+        Console.ReadLine();
+    }
+
+    public static void TraverseTree(string root)
+    {
+        // Data structure to hold names of subfolders to be
+        // examined for files.
+        var dirs = new Stack<string>(20);
+
+        if (!Directory.Exists(root))
+        {
+            throw new ArgumentException();
+        }
+        dirs.Push(root);
+
+        while (dirs.Count > 0)
+        {
+            string currentDir = dirs.Pop();
+            string[] subDirs;
+            try
+            {
+                subDirs = Directory.GetDirectories(currentDir);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+
+            string[] files = null;
+            try
+            {
+                files = Directory.GetFiles(currentDir);
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+
+                Console.WriteLine(e.Message);
+                continue;
+            }
+
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+            // Perform the required action on each file here.
+            // Modify this block to perform your required task.
+            foreach (string file in files)
+            {
+                try
+                {
+                    // Perform whatever action is required in your scenario.
+                    FileInfo fi = new FileInfo(file);
+                    Console.WriteLine("{0}: {1}, {2}", fi.Name, fi.Length, fi.CreationTime);
+                }
+                catch (FileNotFoundException e)
+                {
+                    // If file was deleted by a separate application
+                    //  or thread since the call to TraverseTree()
+                    // then just continue.
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+
+            // Push the subdirectories onto the stack for traversal.
+            // This could also be done before handing the files.
+            foreach (string str in subDirs)
+                dirs.Push(str);
+        }
+    }
+}
