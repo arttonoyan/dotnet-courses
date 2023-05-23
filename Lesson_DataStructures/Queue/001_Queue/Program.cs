6a55bba14728fa5bd5b9e0468c24e5506f56afbe
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-23 14:40:00 +0400

    Add 001_Queue

---
+++ Lesson_DataStructures/Queue/001_Queue/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _001_Queue;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //268435456 -> 1GB
+        var queue = new Queue<int>();
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            queue.Enqueue(i);
+        }
+    }
+}
