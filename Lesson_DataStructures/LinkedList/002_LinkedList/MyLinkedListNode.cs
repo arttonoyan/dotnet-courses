141c422438f7ed0c7656713ff82e631e36cf164e
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-23 14:08:30 +0400

    Add 002_LinkedList

---
+++ Lesson_DataStructures/LinkedList/002_LinkedList/MyLinkedListNode.cs
@@ -0,0 +1,36 @@
+﻿namespace _002_LinkedList
+{
+    public sealed class MyLinkedListNode<T>
+    {
+        internal MyLinkedListNode<T>? next;
+        internal MyLinkedListNode<T>? prev;
+
+        public MyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        internal MyLinkedListNode(MyLinkedList<T> list, T value)
+        {
+            List = list;
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public MyLinkedList<T>? List { get; internal set; }
+
+        public MyLinkedListNode<T> Next 
+            => next == null || next == List.head ? null : next;
+
+        public MyLinkedListNode<T> Previous 
+            => prev == null || this == List.head ? null : prev;
+
+        internal void Invalidate()
+        {
+            List = null;
+            next = null;
+            prev = null;
+        }
+    }
+}
---
+++ Lesson_DataStructures/LinkedList/002_LinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _002_LinkedList;
+
+//https://referencesource.microsoft.com/#System/compmod/system/collections/generic/linkedlist.cs,08181ebdd4cdf907
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new MyLinkedList<int>();
+        list.AddLast(10);
+        list.AddLast(20);
+        var node = list.AddLast(30);
+
+        //MyLinkedListNode<int> node = new MyLinkedListNode<int>(list, -1000);
+        list.AddAfter(node, -1000);
+
+        foreach (int value in list)
+        {
+            Console.WriteLine(value);
+        }
+
+        MyLinkedListNode<int> node30 = list.Find(30);
+        MyLinkedListNode<int> node20 = list.FindLast(20);
+
+        if (list.Contains(30))
+        {
+
+        }
+
+        int[] arr = new int[list.Count];
+        list.CopyTo(arr, 0);
+
+        Array array = new object[list.Count];
+        list.CopyTo(array, 0);
+
+        list.RemoveFirst();
+        list.RemoveLast();
+
+        list.Clear();
+
+        Console.ReadLine();
+    }
+}
