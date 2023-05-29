ecc332c1fd46e37bb22aa3793e2a44aefeb6f40e
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-29 18:06:01 +0400

    Add 003_HashSet project

---
+++ Lesson_DataStructures/HashSet/003_HashSet/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _003_HashSet;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var set = new HashSet<int>();
+        set.Add(10);
+        set.Add(20);
+        set.Add(10);
+        set.Add(30);
+        set.Add(10);
+        set.Add(50);
+
+        set.ExceptWith(new List<int>(2) { 10, 20 });
+    }
+}
