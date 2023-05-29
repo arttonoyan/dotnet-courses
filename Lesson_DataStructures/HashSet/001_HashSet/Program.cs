53d77a23fa290d74f2fb47222d9180755af12057
Artyom Tonoyan <arttonoyan@gmail.com>
2023-05-29 18:01:36 +0400

    Add 001_HashSet project

---
+++ Lesson_DataStructures/HashSet/001_HashSet/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _001_HashSet;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //95991737
+        HashSet<int> set = new HashSet<int>();
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            set.Add(i);
+        }
+    }
+}
