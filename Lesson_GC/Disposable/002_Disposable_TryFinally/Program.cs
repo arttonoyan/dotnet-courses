9663819de5e90d4410fa37d598b469a625d2ebd2
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 12:53:34 +0400

    Add Disposable_TryFinally project

---
+++ Lesson_GC/Disposable/002_Disposable_TryFinally/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _002_Disposable_TryFinally;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var ex = new Example();
+
+        try
+        {
+            ex.SayHello();
+        }
+        finally
+        {
+            if (ex is IDisposable && ex != null)
+            {
+                ex.Dispose();
+            }
+        }
+
+        Console.WriteLine("Done");
+    }
+}
+
+public class Example : IDisposable
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose called");
+    }
+}
