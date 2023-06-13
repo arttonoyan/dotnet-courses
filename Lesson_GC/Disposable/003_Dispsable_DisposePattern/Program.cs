f7903e664f2f4f2976e3f4af5818b1a4559408f7
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 12:57:59 +0400

    Add 003_Dispsable_DisposePattern project

---
+++ Lesson_GC/Disposable/003_Dispsable_DisposePattern/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _003_Dispsable_DisposePattern;
+
+//https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose#implement-the-dispose-pattern
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var ex = new Example();
+        for (int i = 0; i < 10; i++)
+        {
+            ex.Dispose();
+        }
+
+        Console.WriteLine("Done");
+
+        Console.ReadLine();
+    }
+}
+
+public class Example : IDisposable
+{
+    private bool disposed = false;
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Dispose called");
+            }
+
+            disposed = true;
+        }
+    }
+    ~Example()
+    {
+        Dispose(false);
+    }
+}
