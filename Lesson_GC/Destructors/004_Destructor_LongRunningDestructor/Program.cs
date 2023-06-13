22f0247945a1d568834249a99b71f2cc75dc91af
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 11:10:25 +0400

    Add 004_Destructor_LongRunningDestructor project

---
+++ Lesson_GC/Destructors/004_Destructor_LongRunningDestructor/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _004_Destructor_LongRunningDestructor;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Started");
+
+        for (int i = 0; i < 500000; i++)
+        {
+            var ex = new Example(i);
+        }
+
+        Console.WriteLine("Finished");
+
+        Console.ReadLine();
+    }
+
+    public class Example
+    {
+        private readonly int _number;
+        public Example(int number)
+        {
+            _number = number;
+        }
+
+        ~Example()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"Number {_number}. I died...");
+            }
+        }
+    }
+}
