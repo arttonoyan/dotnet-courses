8e6db12598a0f87a45df6a927f03737fd72c1eef
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 12:48:55 +0400

    Add 001_Disposable project

---
+++ Lesson_GC/Disposable/001_Disposable/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _001_Disposable;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var ex = new Example();
+
+        Console.WriteLine("Hi from using");
+        throw new Exception();
+
+        Console.WriteLine("Done");
+
+        Console.ReadLine();
+    }
+}
+
+//public struct Example : IDisposable
+public class Example : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose called");
+    }
+}
