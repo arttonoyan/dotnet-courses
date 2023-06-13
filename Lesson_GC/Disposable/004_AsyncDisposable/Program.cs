8900cd42a78332a41ca49dcdb28d8e60294adf97
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 13:07:33 +0400

    Add 004_AsyncDisposable project

---
+++ Lesson_GC/Disposable/004_AsyncDisposable/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _004_AsyncDisposable;
+
+//https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-disposeasync
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var exampleAsyncDisposable = new ExampleAsyncDisposable();
+        await using (exampleAsyncDisposable.ConfigureAwait(false))
+        {
+            // Interact with the exampleAsyncDisposable instance.
+        }
+
+        Console.ReadLine();
+    }
+}
+public class ExampleAsyncDisposable : IAsyncDisposable
+{
+    private IAsyncDisposable? _example;
+
+    public ExampleAsyncDisposable() =>
+        _example = new NoopAsyncDisposable();
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore().ConfigureAwait(false);
+
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (_example is not null)
+        {
+            await _example.DisposeAsync().ConfigureAwait(false);
+        }
+
+        _example = null;
+    }
+}
+
+public sealed class NoopAsyncDisposable : IAsyncDisposable
+{
+    ValueTask IAsyncDisposable.DisposeAsync() => ValueTask.CompletedTask;
+}
