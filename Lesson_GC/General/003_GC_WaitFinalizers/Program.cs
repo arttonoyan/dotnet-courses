0935db6526684940b2ab17be2c55c04b664ca599
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 11:51:47 +0400

    Add 003_GC_WaitFinalizers project

---
+++ Lesson_GC/General/003_GC_WaitFinalizers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _003_GC_WaitFinalizers;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            var ex = new Example();
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        for (int i = 0; i < 60; i++)
+        {
+            Console.Write(".");
+        }
+    }
+}
+
+public class Example
+{
+    ~Example()
+    {
+        for (int i = 0; i < 80; i++)
+        {
+            Console.Write("-");
+        }
+    }
+}
