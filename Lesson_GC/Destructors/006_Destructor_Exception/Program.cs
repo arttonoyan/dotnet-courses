a530d9b27b6ad9a674951b7d427b74cf43f58525
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 11:35:11 +0400

    Add 006_Destructor_Exception

---
+++ Lesson_GC/Destructors/006_Destructor_Exception/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _006_Destructor_Exception;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Started.");
+
+        for (int i = 0; i < 500000; i++)
+        {
+            var ex = new Example(i);
+        }
+
+        Console.WriteLine("Finished.");
+
+        Console.ReadLine();
+    }
+
+    public class Example
+    {
+        private int _number;
+        public Example(int number)
+        {
+            _number = number;
+        }
+
+        ~Example()
+        {
+            throw new Exception();
+            Console.WriteLine($"Number {_number}. I died...");
+
+            //try
+            //{
+            //
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine($"My number is {_number}. I died...");
+            //}
+        }
+    }
+}
