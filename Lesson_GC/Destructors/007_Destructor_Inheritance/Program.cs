cb46ee1ebde0574eed95fb7f8fcd3bc986662956
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 11:38:55 +0400

    Add 007_Destructor_Inheritance

---
+++ Lesson_GC/Destructors/007_Destructor_Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _007_Destructor_Inheritance;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Started.");
+
+        for (int i = 0; i < 500000; i++)
+        {
+            var ex = new Example(i);
+        }
+
+        Console.WriteLine("Finished.");
+
+        Console.ReadLine();
+    }
+}
+
+public class BaseExample
+{
+    protected int _number;
+    public BaseExample(int number)
+    {
+        _number = number;
+    }
+
+    ~BaseExample()
+    {
+        Console.WriteLine($"BASE CLASS DESTRUCTOR NUMBER {_number}.");
+        //base.Finalize();
+    }
+}
+public class Example : BaseExample
+{
+    public Example(int number) : base(number)
+    {
+    }
+
+    ~Example()
+    {
+        Console.WriteLine($"Number {_number}. I died...");
+    }
+}
