653eeb00d73dc7962ffc684609d1a927d2623956
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 10:33:58 +0400

    Add 002_Destructor_CallAndPrint project

---
+++ Lesson_GC/Destructors/002_Destructor_CallAndPrint/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _002_Destructor_CallAndPrint;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 500000; i++)
+        {
+            var ex = new Example(i);
+        }
+
+        Console.ReadLine();
+    }
+
+    public class Example
+    {
+        private readonly int _number;
+        public Example(int number)
+        {
+            _number = number;
+        }
+
+        ~Example()
+        {
+            Console.WriteLine($"Number {_number}. I died...");
+        }
+    }
+}
