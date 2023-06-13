96c5b7dbc56b63ff9ed31716cdd52de19089befa
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-13 11:45:12 +0400

    Add 001_GC_LargeObjectGen project

---
+++ Lesson_GC/General/001_GC_LargeObjectGen/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _001_GC_LargeObjectGen;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //byte[] data = new byte[85];
+        byte[] data = new byte[85000];
+        //var data = new BigExample();
+
+        var gen = GC.GetGeneration(data);
+        Console.WriteLine(gen);
+
+        Console.ReadLine();
+    }
+}
+
+public class BigExample
+{
+    // 1000000 byte = 9766 KB = 9.5 MB
+    byte[] arr = new byte[10000000];
+}
+
+public class Example
+{
+    ~Example()
+    {
+        Console.WriteLine("Example died...");
+    }
+}
