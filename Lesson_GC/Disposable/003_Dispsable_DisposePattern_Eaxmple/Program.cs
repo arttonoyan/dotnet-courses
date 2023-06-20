3a0d7773cf3b4fe056000876806425d900222011
Artyom Tonoyan <arttonoyan@gmail.com>
2023-06-20 09:12:44 +0400

    Add 003_Dispsable_DisposePattern_Eaxmple project

---
+++ Lesson_GC/Disposable/003_Dispsable_DisposePattern_Eaxmple/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _003_Dispsable_DisposePattern_Eaxmple;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Main thread Id is {Thread.CurrentThread.ManagedThreadId}");
+
+        Console.WriteLine("Enter any key to continue.");
+        Console.ReadLine();
+
+        for (int i = 0; i < 500000; i++)
+        {
+            /*using*/ var ex = new Example(i);
+        }
+
+        Console.WriteLine("Finished.");
+        Console.ReadLine();
+    }
+}
+
+public class Example : IDisposable
+{
+    private readonly int _number;
+    public Example(int number)
+    {
+        _number = number;
+    }
+
+
+    private bool disposed = false;
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Dispose called");
+            }
+
+            disposed = true;
+        }
+    }
+    ~Example()
+    {
+        Console.WriteLine($"Number {_number}. Thread Id is {Thread.CurrentThread.ManagedThreadId} I died...");
+        Dispose(false);
+    }
+}
