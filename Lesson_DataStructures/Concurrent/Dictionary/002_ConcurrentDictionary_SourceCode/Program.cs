799d511a6c3252907283d29a0007a073d1224fd0
Artyom Tonoyan <arttonoyan@gmail.com>
2023-07-04 15:56:43 +0400

    Add _002_ConcurrentDictionary_SourceCode

---
+++ Lesson_DataStructures/Concurrent/Dictionary/002_ConcurrentDictionary_SourceCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _002_ConcurrentDictionary_SourceCode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var dict = new MyConcurrentDictionary<int, string>();
+        var res1 = dict.TryAdd(1, "First");
+    }
+}
