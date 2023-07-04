28cb7167a34b58df079db1daa736ea5cca46d852
Artyom Tonoyan <arttonoyan@gmail.com>
2023-07-04 16:06:30 +0400

    Update sample

--- Lesson_DataStructures/Concurrent/Dictionary/002_ConcurrentDictionary_SourceCode/Program.cs
+++ Lesson_DataStructures/Concurrent/Dictionary/002_ConcurrentDictionary_SourceCode/Program.cs
@@ -5,6 +5,8 @@ internal class Program
     static void Main(string[] args)
     {
         var dict = new MyConcurrentDictionary<int, string>();
-        var res1 = dict.TryAdd(1, "First");
+        bool res1 = dict.TryAdd(1, "First");
+        bool res2 = dict.TryAdd(2, "Second");
+        bool res3 = dict.TryAdd(1, "First");
     }
 }
