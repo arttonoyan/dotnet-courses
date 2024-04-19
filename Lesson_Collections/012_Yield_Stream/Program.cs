071d146e4ade615058f2a401d605780646612714
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-04-19 10:57:16 +0400

    refactor(012_Yield_Stream): Add 'using' directive for namespace inclusion.

--- Lesson_Collections/012_Yield_Stream/Program.cs
+++ Lesson_Collections/012_Yield_Stream/Program.cs
@@ -12,14 +12,12 @@ namespace _012_Yield_Stream
     {
         static void Main(string[] args)
         {
-            FileStream stream = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
+            using FileStream stream = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
 
             var allBytes1 = stream.AsEnumerable().ToArray();
             var allBytes2 = File.ReadAllBytes("test.txt");
 
             bool isTrue = Equals(allBytes1, allBytes2);
-
-            stream.Dispose();
 
             Console.ReadLine();
         }
